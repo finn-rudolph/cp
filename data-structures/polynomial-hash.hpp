af367c5d6bcf46a7f1453c224e9558f2d5d38a87
Finn Rudolph <finn.rudolph@t-online.de>
2023-03-03 10:31:59 +0100

    Add Polynomial Hash

---
+++ data-structures/polynomial-hash.hpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Allows to compute the polynomial hash of any substring in O(1) with O(n)
+preprocessing. The template argument for mod should be a large prime, e. g.
+10^9 + 9 or 2^61 - 1. T must be signed and able to represent mod^2.
+*/
+
+template <typename T, T base, T mod>
+struct PolynomialHash
+{
+    vector<T> z, p;
+
+    PolynomialHash(string const &s)
+    {
+        z = vector<T>(s.size());
+        p = vector<T>(s.size());
+        p[0] = 1;
+        z[0] = s[0];
+        for (size_t i = 1; i < s.size(); i++)
+        {
+            z[i] = (z[i - 1] * base + s[i]) % mod;
+            p[i] = (p[i - 1] * base) % mod;
+        }
+    }
+
+    T range_hash(size_t i, size_t j) // hash value in [i, j)
+    {
+        if (!i)
+            return z[j - 1];
+        return (z[j - 1] - (z[i - 1] * p[j - i]) % mod + mod) % mod;
+    }
+};
