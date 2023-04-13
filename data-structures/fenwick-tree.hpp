7c1b3367480ff654637c0e3489bcf6b63495637c
Finn Rudolph <finn.rudolph@t-online.de>
2023-04-13 16:55:10 +0200

    Implement Fenwick Trees with TMP

--- data-structures/fenwick-tree.hpp
+++ data-structures/fenwick-tree.hpp
@@ -2,88 +2,96 @@
 using namespace std;
 
 /*
-A standard one-dimensional Fenwick Tree plus one supporting range updates
-and range queries. Both take O(log n) time for each type of query.
+An arbitrary-dimensional Fenwick Tree for point update range query, as well
+as for range update range query. Everything is 0-indexed.
+Point update range query runs in O(log n_1 * log n_2 * ... * log n_d) per query,
+where n_i is the size of the i-th dimension and d the number of dimensions.
+Range update range query takes O(4^d * log n_1 * log n_2 * ... * log n_d) time.
 */
 
-template <typename T>
+template <typename T, int64_t... Ns>
 struct FenwickTree
 {
-    vector<T> t;
+    T t = 0;
+    void increment(T x) { t += x; }
+    T rangeSum() { return t; }
+};
 
-    FenwickTree() {}
+template <typename T, int64_t N, int64_t... Ns>
+struct FenwickTree<T, N, Ns...>
+{
+    FenwickTree<T, Ns...> t[N];
 
-    FenwickTree(size_t n)
+    template <typename... Args>
+    void increment(int64_t i, Args... a)
     {
-        t = vector<T>(n, 0);
+        ++i;
+        while (i <= N)
+            t[i - 1].increment(a...), i += i & -i;
     }
 
-    T prefix_query(int64_t i)
+    template <typename... Args>
+    T rangeSum(int64_t i, int64_t j, Args... a)
     {
-        i++;
         T x = 0;
+        ++j;
+        while (j)
+            x += t[j - 1].rangeSum(a...), j -= j & -j;
         while (i)
+            x -= t[i - 1].rangeSum(a...), i -= i & -i;
+        return x;
+    }
+};
+
+template <typename T, int64_t... Ns>
+struct FenwickTreeRURQ
+{
+    T t = 0;
+    void increment(T x) { t += x; }
+    T rangeSum() { return t; }
+};
+
+template <typename T, int64_t N, int64_t... Ns>
+struct FenwickTreeRURQ<T, N, Ns...>
+{
+    FenwickTreeRURQ<T, Ns...> t[2][N];
+
+    template <typename... Args>
+    void suffixIncrement(T x, int64_t i, Args... args)
+    {
+        int64_t k = i + 1;
+        while (k <= N)
         {
-            x += t[i - 1];
-            i -= i & -i;
+            t[0][k - 1].increment(x, args...);
+            t[1][k - 1].increment(x * (T)i, args...);
+            k += k & -k;
+        }
+    }
+
+    template <typename... Args>
+    void increment(T x, int64_t i, int64_t j, Args... args)
+    {
+        suffixIncrement(x, i, args...);
+        suffixIncrement(-x, j + 1, args...);
+    }
+
+    template <typename... Args>
+    T prefixSum(int64_t i, Args... args)
+    {
+        T x = 0;
+        int64_t k = i + 1;
+        while (k)
+        {
+            x += (T)(i + 1) * t[0][k - 1].rangeSum(args...);
+            x -= t[1][k - 1].rangeSum(args...);
+            k -= k & -k;
         }
         return x;
     }
 
-    T range_query(int64_t i, int64_t j)
+    template <typename... Args>
+    T rangeSum(int64_t i, int64_t j, Args... args)
     {
-        return prefix_query(j) - (i ? prefix_query(i - 1) : 0);
-    }
-
-    void increment(int64_t i, T x)
-    {
-        i++;
-        while ((size_t)i <= t.size())
-        {
-            t[i - 1] += x;
-            i += i & -i;
-        }
-    }
-
-    void set(int64_t i, T x)
-    {
-        increment(i, x - range_query(i, i));
-    }
-};
-
-template <typename T>
-struct FenwickTreeRURQ
-{
-    array<FenwickTree<T>, 2> t;
-
-    FenwickTreeRURQ() {}
-
-    FenwickTreeRURQ(size_t n)
-    {
-        t[0] = FenwickTree<T>(n);
-        t[1] = FenwickTree<T>(n);
-    }
-
-    T prefix_query(int64_t i)
-    {
-        return t[0].prefix_query(i) * (i + 1) - t[1].prefix_query(i);
-    }
-
-    T range_query(int64_t i, int64_t j)
-    {
-        return prefix_query(j) - (i ? prefix_query(i - 1) : 0);
-    }
-
-    void suffix_increment(int64_t i, T x)
-    {
-        t[0].increment(i, x);
-        t[1].increment(i, x * i);
-    }
-
-    void increment(int64_t i, int64_t j, T x)
-    {
-        suffix_increment(i, x);
-        if ((size_t)j + 1 < t[0].t.size())
-            suffix_increment(j + 1, -x);
+        return prefixSum(j, args...) - (i ? prefixSum(i - 1, args...) : 0);
     }
 };
