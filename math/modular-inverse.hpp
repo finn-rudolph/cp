1f251b8c16cbe9050a377831b73c28d7883a1c8a
Finn Rudolph <finn.rudolph@t-online.de>
2023-04-15 09:05:03 +0200

    Add extended euclidean algorithm

--- math/modular-inverse.hpp
+++ math/modular-inverse.hpp
@@ -19,3 +19,12 @@ T binary_exp(T x, T y, T m)
 
 template <typename T>
 T mod_inv(T x, T m) { return binary_exp(x, m - 2, m); }
+
+template <typename T>
+tuple<T, T, T> extended_euclid(T a, T b)
+{
+    if (!b)
+        return {a, 1, 0};
+    auto const [g, s, t] = extended_euclid(b, a % b);
+    return {g, t, s - a / b * t};
+}
