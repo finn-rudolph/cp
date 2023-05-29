a8c554381f6be3b210263253d7edefdd3c24d41f
Finn Rudolph <finn.rudolph@t-online.de>
2023-05-29 15:48:03 +0200

    Make modulus in modular inverse code ICE

--- math/modular-inverse.hpp
+++ math/modular-inverse.hpp
@@ -1,16 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+Modular inverse via binary exponentiation.
+*/
+
 template <typename T>
-T binary_exp(T x, T y, T m)
+T binary_exp(T x, T y)
 {
     T z = 1;
 
     while (y)
     {
         if (y & 1)
-            z = (z * x) % m;
-        x = (x * x) % m;
+            z = (z * x) % Mod;
+        x = (x * x) % Mod;
         y >>= 1;
     }
 
@@ -18,7 +22,12 @@ T binary_exp(T x, T y, T m)
 }
 
 template <typename T>
-T mod_inv(T x, T m) { return binary_exp(x, m - 2, m); }
+T modular_inverse(T x) { return binary_exp(x, Mod - 2); }
+
+/*
+Modular inverse via extended euclidean algorithm, also works for composite
+(but coprime) moduli.
+*/
 
 template <typename T>
 tuple<T, T, T> extended_euclid(T a, T b)
@@ -26,5 +35,8 @@ tuple<T, T, T> extended_euclid(T a, T b)
     if (!b)
         return {a, 1, 0};
     auto const [g, s, t] = extended_euclid(b, a % b);
-    return {g, t, s - a / b * t};
+    return {g, t, (s - (a / b * t) % Mod + Mod) % Mod};
 }
+
+template <typename T>
+T modular_inverse(T x) { return get<1>(extended_euclid(x, Mod)); }
